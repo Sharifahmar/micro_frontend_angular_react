fc0da3929de7bc3b814e3a9e5b78db6ce415683e
Your Name <you@example.com>
2021-05-04 01:02:38 +0530

    command to run microfrontend

---
+++ run.bat
@@ -0,0 +1,9 @@
+:: cd ng directory, and start angular app
+cd ng 
+start cmd.exe /k "npm start"
+:: cd react-app directory, and start react app
+cd ../react-app 
+start cmd.exe /k "npm start"
+:: cd root-html directory, and start common microfront end app 
+cd ../root-html 
+start cmd.exe /k "npm start"
